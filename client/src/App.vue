db12cf8cad7aa8c5c257458e6835a1fb15a0f95e
tshea <Tylershea12@gmail.com>
2023-11-04 15:35:44 -0500

    update vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,38 +1,85 @@
-<template>
-    <v-app>
-      <app-navigation></app-navigation>
-      <v-content transition="slide-x-transition">
-        <router-view></router-view>
-      </v-content>
-      <app-tabs
-        v-if="isLoggedIn(true)"
-      ></app-tabs>
-    </v-app>
-</template>
-
-<script>
-import { mapActions, mapState } from 'vuex';
-import AppNavigation from '@/components/AppNavigation.vue';
-import AppTabs from '@/components/AppTabs.vue';
-
-export default {
-  name: 'App',
-  components: {
-    AppNavigation,
-    AppTabs,
-  },
-  computed: {
-    ...mapState([
-      'account',
-    ]),
-  },
-  methods: {
-    isLoggedIn(accountOnly) {
-      return (this.account.email && accountOnly) || (!this.account.email && !accountOnly);
-    },
-  },
-};
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style>
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
 </style>
