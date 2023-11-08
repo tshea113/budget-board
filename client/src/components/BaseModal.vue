bffc1aff122758db15f5c62f2cf41fdf1cc53341
tshea <Tylershea12@gmail.com>
2023-11-07 22:25:11 -0600

    fix up login

--- client/src/components/BaseModal.vue
+++ client/src/components/BaseModal.vue
@@ -1,18 +1,20 @@
 <template>
   <div
     v-show="modalActive"
-    class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+    class="absolute left-0 top-0 flex h-screen w-full justify-center bg-black bg-opacity-30"
   >
-    <div v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-screen-md rounded-lg">
-      <slot></slot>
-      <button class="text-white mt-8 bg-blue-400 py-2 px-6 rounded-md" @click="$emit('closeModal')">
-        close
+    <div v-if="modalActive" class="mt-32 max-w-2xl flex-col self-start rounded-lg bg-white p-4">
+      <button class="" @click="$emit('closeModal')">
+        <XMarkIcon class="h-6 w-6 justify-end hover:text-indigo-600"></XMarkIcon>
       </button>
+      <slot></slot>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { XMarkIcon } from '@heroicons/vue/24/solid'
+
 defineEmits(['closeModal'])
 defineProps({
   modalActive: {
