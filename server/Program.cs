17dfdeb5e4615eef9f4775073a355eb1f35adbac
tshea <Tylershea12@gmail.com>
2023-11-10 21:02:45 -0600

    Basic backend fallback

--- server/Program.cs
+++ server/Program.cs
@@ -3,26 +3,29 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
 app.MapControllers();
+
+app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Spa}");
+
+app.MapFallbackToFile("index.html");
 
 app.Run();
