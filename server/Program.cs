a9e4806ffd3bb2cf60fa24566aa5430c72a93fd4
tshea <Tylershea12@gmail.com>
2023-11-26 16:07:23 -0600

    fix stuff

--- server/Program.cs
+++ server/Program.cs
@@ -4,11 +4,24 @@ using Google.Apis.Auth.OAuth2;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Set the environment variable using the project secrets
+if (builder.Environment.IsDevelopment())
+{
+    var firebaseConfigSecret = builder.Configuration.GetValue<string>("GOOGLE_APPLICATION_CREDENTIALS");
+    Environment.SetEnvironmentVariable("FIREBASE_CONFIG", firebaseConfigSecret);
+}
+
 // Add services to the container.
+
+var firebaseConfig = Environment.GetEnvironmentVariable("FIREBASE_CONFIG");
+if (firebaseConfig == null)
+{
+    throw new ArgumentNullException(nameof(firebaseConfig));
+}
 
 builder.Services.AddSingleton(FirebaseApp.Create(new AppOptions()
 {
-    Credential = GoogleCredential.FromJson(builder.Configuration.GetValue<string>("FIREBASE_CONFIG"))
+    Credential = GoogleCredential.FromJson(firebaseConfig)
 }));
 builder.Services.AddFirebaseAuthentication();
 builder.Services.AddAuthorization();
