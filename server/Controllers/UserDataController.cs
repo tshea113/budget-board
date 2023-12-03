f6f12bc8e1bdda9c60e08f5b6b2d4e21086b8895
tshea <Tylershea12@gmail.com>
2023-12-02 23:57:53 -0600

    User querying

---
+++ server/Controllers/UserDataController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MoneyMinder.Data;
+using MoneyMinder.Models;
+
+namespace MoneyMinder.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserDataController : ControllerBase
+    {
+        private readonly UserDataContext _userDataContext;
+        public UserDataController(UserDataContext context)
+        {
+            _userDataContext = context;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult CreateUser(UserDetails user)
+        {
+            var newUser = new User
+            {
+                Uid = user.Uid,
+            };
+
+            _userDataContext.Users.Add(newUser);
+            _userDataContext.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetUser(string uid)
+        {
+            var user = _userDataContext.Users.First(x => x.Uid == uid);
+
+            if (user == null)
+            {
+                return BadRequest("User not found!");
+            }
+
+            return Ok(user);
+
+        }
+    }
+}
--- server/MoneyMinder-Data/User.cs
+++ server/MoneyMinder-Data/User.cs
@@ -2,6 +2,7 @@
 {
     public class User
     {
-        public int Uid { get; set; }
+        public int Id { get; set; }
+        public required string Uid { get; set; }
     }
 }
