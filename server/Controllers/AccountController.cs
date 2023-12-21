2d1ae23660279fc6b20ca6d91fcec38eaa2dbcd5
tshea <Tylershea12@gmail.com>
2023-12-20 21:32:32 -0600

    account api

--- server/Controllers/AccountController.cs
+++ server/Controllers/AccountController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MoneyMinder.Database.Data;
+using MoneyMinder.Database.Models;
 
 namespace MoneyMinder.Controllers;
 
-[Route("api/[controller]/[action]")]
+[Route("api/[controller]")]
 [ApiController]
 public class AccountController : ControllerBase
 {
@@ -17,16 +19,109 @@ public class AccountController : ControllerBase
 
     [HttpGet]
     [Authorize]
-    public IActionResult GetAllAccounts()
+    public async Task<IActionResult> Get()
     {
         try
         {
             string uid = User.Claims.Single(c => c.Type == "id").Value;
-            return Ok(_userDataContext.Users.Single(u => u.Uid == uid));
+            User? user = await _userDataContext.Users.FirstOrDefaultAsync(u => u.Uid == uid);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
         }
         catch (Exception ex)
         {
             return BadRequest(ex.Message);
         }
+    }
+
+    [HttpGet("{guid}")]
+    [Authorize]
+    public async Task<IActionResult> Get(Guid guid)
+    {
+        try
+        {
+            string uid = User.Claims.Single(c => c.Type == "id").Value;
+            User? user = await _userDataContext.Users.FirstOrDefaultAsync(u => u.Uid == uid);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpPost]
+    [Authorize]
+    public IActionResult Add([FromBody] Account account)
+    {
+        try
+        {
+            string uid = User.Claims.Single(c => c.Type == "id").Value;
+            User? user = _userDataContext.Users.Single(u => u.Uid == uid);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.Accounts.Add(account);
+            _userDataContext.SaveChanges();
+
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpDelete("{guid}")]
+    [Authorize]
+    public async Task<IActionResult> Delete(Guid guid)
+    {
+        Account? account = await _userDataContext.Accounts.FindAsync(guid);
+        if (account == null)
+        {
+            return NotFound();
+        }
+
+        try
+        {
+            _userDataContext.Accounts.Remove(account);
+            await _userDataContext.SaveChangesAsync();
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpPut]
+    [Authorize]
+    public async Task<IActionResult> Edit([FromBody] Account newAccount)
+    {
+        Account? account = await _userDataContext.Accounts.FindAsync(newAccount.ID);
+        if (account == null)
+        {
+            return NotFound();
+        }
+
+        account.Name = newAccount.Name;
+        account.Institution = newAccount.Institution;
+        account.Type = newAccount.Type;
+        account.Subtype = newAccount.Subtype;
+
+        await _userDataContext.SaveChangesAsync();
+
+        return Ok();
     }
 }
