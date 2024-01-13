9ebab1c1f73bbf2e9a9a835be2db2f61a20f0923
tshea <Tylershea12@gmail.com>
2024-01-13 14:19:06 -0600

    add transaction stuff

--- client/src/Dashboard/add-transaction.tsx
+++ client/src/Dashboard/add-transaction.tsx
@@ -1,24 +1,41 @@
 import { Card } from '@/components/ui/card';
-import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 import { type SubmitHandler, useForm } from 'react-hook-form';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Input } from '@/components/ui/input';
 import ResponsiveButton from '@/components/responsive-button';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { format } from 'date-fns';
+import { CalendarIcon } from 'lucide-react';
+import { Calendar } from '@/components/ui/calendar';
 
 const formSchema = z.object({
+  date: z.date({
+    required_error: 'A transaction date is required.',
+  }),
+  merchant: z.string().min(1).max(50),
   amount: z.number(),
 });
 
 const AddTransaction = (): JSX.Element => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      amount: 0,
-    },
+    defaultValues: {},
   });
 
   interface FormValues {
+    date: Date;
+    merchant: string;
     amount: number;
   }
 
@@ -29,7 +46,7 @@ const AddTransaction = (): JSX.Element => {
   };
 
   return (
-    <Card className="w-screen">
+    <Card className="w-screen p-2">
       <Form {...form}>
         <form
           onSubmit={(event) => {
@@ -38,18 +55,71 @@ const AddTransaction = (): JSX.Element => {
           }}
           className="space-y-8"
         >
-          <FormField
-            control={form.control}
-            name="amount"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Amount</FormLabel>
-                <FormControl>
-                  <Input placeholder="0" {...field} />
-                </FormControl>
-              </FormItem>
-            )}
-          />
+          <div className="flex gap-4">
+            <FormField
+              control={form.control}
+              name="date"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Date</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={'outline'}
+                          className={cn(
+                            'w-[240px] pl-3 text-left font-normal',
+                            // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+                            !field.value && 'text-muted-foreground'
+                          )}
+                        >
+                          {
+                            // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+                            field.value ? format(field.value, 'PPP') : <span>Pick a date</span>
+                          }
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onDayClick={field.onChange}
+                        disabled={(date) => date > new Date() || date < new Date('1900-01-01')}
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="merchant"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Merchant</FormLabel>
+                  <FormControl>
+                    <Input className="w-[240px]" placeholder="Enter a merchant" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Amount</FormLabel>
+                  <FormControl>
+                    <Input className="w-[240px]" placeholder="Enter an amount" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
           <ResponsiveButton />
         </form>
       </Form>
--- client/src/Dashboard/transactions.tsx
+++ client/src/Dashboard/transactions.tsx
@@ -1,51 +1,54 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { formatDate, getTransactions } from "@/lib/transactions";
-import { useQuery } from "@tanstack/react-query";
-import AddTransaction from "./add-transaction";
-import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { formatDate, getTransactions } from '@/lib/transactions';
+import { useQuery } from '@tanstack/react-query';
+import AddTransaction from './add-transaction';
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
 
-type Transaction = {
-  id: string,
-  date: string,
-  merchantName: string,
-  category: string,
-  amount: string
+interface Transaction {
+  id: string;
+  date: string;
+  merchantName: string;
+  category: string;
+  amount: string;
 }
 
-const Transactions = () => {
+const Transactions = (): JSX.Element => {
   const [isOpen, setIsOpen] = useState(false);
   const { isPending, isError, data, error } = useQuery({
     queryKey: ['transactions'],
     queryFn: async () => {
       const response = await getTransactions();
       return response;
-    }
+    },
   });
 
   if (isPending) {
-    return <span>Loading...</span>
+    return <span>Loading...</span>;
   }
 
   if (isError) {
-    return <span>Error: {error.message}</span>
+    return <span>Error: {error.message}</span>;
   }
 
-  const toggle = () => {
+  const toggle = (): void => {
     setIsOpen((isOpen) => !isOpen);
-  }
+  };
 
   return (
     <Card className="">
       <CardHeader className="grid w-screen grid-cols-2">
-        <CardTitle className="justify-self-start">
-          Transactions
-        </CardTitle>
+        <CardTitle className="justify-self-start">Transactions</CardTitle>
         <div className="justify-self-end">
-          <Button onClick={toggle}>
-            Add Transaction
-          </Button>
+          <Button onClick={toggle}>Add Transaction</Button>
         </div>
       </CardHeader>
       {isOpen && <AddTransaction />}
@@ -72,7 +75,7 @@ const Transactions = () => {
         </Table>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
 
 export default Transactions;
