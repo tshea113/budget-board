5f5d1a75ce2cc9fde0b95e4abedcd6639a95666f
tshea <Tylershea12@gmail.com>
2024-01-27 15:40:03 -0600

    add alert for unverified email

--- client/src/app/dashboard/dashboard.tsx
+++ client/src/app/dashboard/dashboard.tsx
@@ -1,29 +1,19 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import AccountCard from './account-card';
 import WelcomeCard from './welcome-card';
-import { useContext, useState } from 'react';
+import { useState } from 'react';
 import AddAccount from './add-account';
-import { Alert, AlertDescription } from '@/components/ui/alert';
-import { AlertCircle } from 'lucide-react';
-import { AuthContext } from '@/components/auth-provider';
+import EmailVerified from './email-verified';
 
 const Dashboard = (): JSX.Element => {
   const [addAccountIsOpen, setAddAccountIsOpen] = useState(false);
-  const { emailVerified } = useContext<any>(AuthContext);
 
   const toggleAddAccount = (): void => {
     setAddAccountIsOpen((addAccountIsOpen) => !addAccountIsOpen);
   };
   return (
     <div>
-      {!emailVerified && (
-        <Alert variant="destructive">
-          <AlertCircle className="h-4 w-4" />
-          <AlertDescription>
-            Before you can get started you will need to check your email for a validation link.
-          </AlertDescription>
-        </Alert>
-      )}
+      <EmailVerified />
       <div className="flex flex-col gap-2 lg:grid lg:grid-flow-col lg:grid-cols-3 lg:grid-rows-5">
         <div className="lg:row-span-full">
           <AccountCard toggleAddAccount={toggleAddAccount} />
