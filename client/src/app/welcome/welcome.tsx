3f3a72d6689f093a996c43a607d06a964c24b226
tshea <Tylershea12@gmail.com>
2024-02-01 17:37:53 -0600

    Rename client code to budget board

--- client/src/app/welcome/welcome.tsx
+++ client/src/app/welcome/welcome.tsx
@@ -6,7 +6,7 @@ const Welcome = (): JSX.Element => {
   return (
     <div className="mx-auto max-w-2xl py-32 text-center sm:py-48 lg:py-56">
       <h3 className="text-2xl tracking-tight">Welcome to</h3>
-      <h1 className="text-4xl font-bold sm:text-6xl">Money Minder</h1>
+      <h1 className="text-4xl font-bold sm:text-6xl">Budget Board</h1>
       <p className="mt-6 text-lg leading-8">A simple app for managing monthly budgets.</p>
       <div className="mt-10 flex items-center justify-center gap-x-6">
         <Modal button="Login">
