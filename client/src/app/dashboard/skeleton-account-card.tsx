75521137dedd31e7f0690b9c8dbfcd002e8b0360
tshea113 <Tylershea12@gmail.com>
2024-02-22 14:58:47 -0800

    account card skeleton

---
+++ client/src/app/dashboard/skeleton-account-card.tsx
@@ -0,0 +1,16 @@
+import { Skeleton } from '@/components/ui/skeleton';
+import { Card } from '../../components/ui/card';
+
+const SkeletonAccountCard = (): JSX.Element => {
+  return (
+    <Card>
+      <div className="m-3 flex flex-col space-y-3">
+        <Skeleton className="h-10 max-w-[125px]" />
+        <Skeleton className="h-[250px] max-w-[500px] rounded-xl" />
+        <Skeleton className="h-10 max-w-[100px]" />
+      </div>
+    </Card>
+  );
+};
+
+export default SkeletonAccountCard;
