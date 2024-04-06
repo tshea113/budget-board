2ab72600de534c926af8d1684303c8309b92fa15
tshea <Tylershea12@gmail.com>
2024-04-06 16:10:06 -0500

    move some stuff

--- client/src/lib/budgets.ts
+++ client/src/lib/budgets.ts
@@ -1,5 +1,11 @@
 import { type AxiosResponse } from 'axios';
 import request from './request';
+import { type Budget } from '@/types/budget';
+
+export enum BudgetGroup {
+  Income,
+  Spending,
+}
 
 export const getBudgets = async (date: Date): Promise<AxiosResponse> => {
   const dateString = '?date=' + date.toISOString();
@@ -7,4 +13,19 @@ export const getBudgets = async (date: Date): Promise<AxiosResponse> => {
     url: '/api/budget' + dateString,
     method: 'GET',
   });
+};
+
+export const parseBudgetGroups = (
+  budgetData: Budget[] | undefined,
+  budgetGroup: BudgetGroup
+): Budget[] => {
+  if (budgetData == null) return [];
+
+  if (budgetGroup === BudgetGroup.Income) {
+    return budgetData.filter((b) => b.category === 'income') ?? [];
+  } else if (budgetGroup === BudgetGroup.Spending) {
+    return budgetData.filter((b) => b.category !== 'income') ?? [];
+  } else {
+    return budgetData;
+  }
 };
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
+
+export const initMonth = (): Date => {
+  const date = new Date();
+  date.setDate(1);
+  return date;
+};
