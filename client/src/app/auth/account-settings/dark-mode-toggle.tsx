84393e573241ffe864a900ffbe9968300ade2a5b
tshea <Tylershea12@gmail.com>
2024-05-27 19:21:52 -0500

    Add a dark mode toggle to account settings

---
+++ client/src/app/auth/account-settings/dark-mode-toggle.tsx
@@ -0,0 +1,30 @@
+import { useTheme } from '@/components/theme-provider';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+const DarkModeToggle = (): JSX.Element => {
+  const { setTheme, theme } = useTheme();
+
+  return (
+    <div className="flex flex-col space-y-2 p-2">
+      <span>Visual Appearance</span>
+      <Select onValueChange={setTheme} defaultValue={theme}>
+        <SelectTrigger>
+          <SelectValue placeholder="Select a theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+export default DarkModeToggle;
