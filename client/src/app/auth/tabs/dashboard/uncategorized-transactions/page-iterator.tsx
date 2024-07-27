ea42e91f2d5c3d8a3040ac8b4cc4effb44b54564
tshea <Tylershea12@gmail.com>
2024-07-27 16:39:20 -0500

    Add page turning

--- client/src/app/auth/tabs/dashboard/uncategorized-transactions/page-iterator.tsx
+++ client/src/app/auth/tabs/dashboard/uncategorized-transactions/page-iterator.tsx
@@ -5,10 +5,20 @@ import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
 interface PageIteratorProps {
   className?: string;
   page: number;
+  maxPages: number;
   setPage: (page: number) => void;
 }
 
 const PageIterator = (props: PageIteratorProps): JSX.Element => {
+  const changePage = (newPage: number) => {
+    if (newPage < 1) {
+      props.setPage(props.maxPages);
+    } else if (newPage > props.maxPages) {
+      props.setPage(1);
+    } else {
+      props.setPage(newPage);
+    }
+  };
   return (
     <div
       className={cn(
@@ -16,19 +26,13 @@ const PageIterator = (props: PageIteratorProps): JSX.Element => {
         props.className
       )}
     >
-      <Button
-        className="p-2"
-        variant="ghost"
-        onClick={() => props.setPage(props.page - 1)}
-      >
+      <Button className="p-2" variant="ghost" onClick={() => changePage(props.page - 1)}>
         <ChevronLeftIcon />
       </Button>
-      <div className="text-lg font-semibold">Page {props.page}</div>
-      <Button
-        className="p-2"
-        variant="ghost"
-        onClick={() => props.setPage(props.page + 1)}
-      >
+      <div className="text-lg font-semibold">
+        Page {props.page} of {props.maxPages}
+      </div>
+      <Button className="p-2" variant="ghost" onClick={() => changePage(props.page + 1)}>
         <ChevronRightIcon />
       </Button>
     </div>
