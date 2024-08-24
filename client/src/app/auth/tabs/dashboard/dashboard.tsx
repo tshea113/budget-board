3383862aea1a76729c5fb0e35ed9c56d19694e2a
tshea <Tylershea12@gmail.com>
2024-08-23 21:37:42 -0500

    cap the dashboard to 2xl screen size

--- client/src/app/auth/tabs/dashboard/dashboard.tsx
+++ client/src/app/auth/tabs/dashboard/dashboard.tsx
@@ -6,13 +6,13 @@ import UncategorizedTransactionsCard from './uncategorized-transactions/uncatego
 
 const Dashboard = (): JSX.Element => {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <EmailVerified />
-      <div className="flex flex-col gap-2 lg:grid lg:grid-flow-col lg:grid-cols-10 lg:grid-rows-5">
+      <div className="flex flex-col gap-2 lg:grid lg:grid-flow-col lg:grid-cols-10 lg:grid-rows-5 2xl:max-w-screen-2xl">
         <div className="col-span-3 row-span-full">
           <AccountCard />
         </div>
-        <div className="col-span-8 space-y-2">
+        <div className="col-span-7 space-y-2">
           <WelcomeCard />
           <UncategorizedTransactionsCard />
         </div>
