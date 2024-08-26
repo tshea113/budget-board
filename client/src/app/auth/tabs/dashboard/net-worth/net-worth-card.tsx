36c5c9bf32e9b67c22f4c90997c749eeb78477f6
tshea <Tylershea12@gmail.com>
2024-08-26 18:22:08 -0500

    Add net worth card

---
+++ client/src/app/auth/tabs/dashboard/net-worth/net-worth-card.tsx
@@ -0,0 +1,69 @@
+import { Card } from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
+import SkeletonAccountCard from '../accounts/skeleton-account-card';
+import { AxiosResponse } from 'axios';
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { AuthContext } from '@/components/auth-provider';
+import { Account } from '@/types/account';
+import NetWorthItem from './net-worth-item';
+
+const NetWorthCard = (): JSX.Element => {
+  const { request } = React.useContext<any>(AuthContext);
+  const accountsQuery = useQuery({
+    queryKey: ['accounts'],
+    queryFn: async (): Promise<Account[]> => {
+      const res: AxiosResponse = await request({
+        url: '/api/account',
+        method: 'GET',
+      });
+
+      if (res.status == 200) {
+        return res.data;
+      }
+
+      return [];
+    },
+  });
+
+  const validAccounts = React.useMemo(() => {
+    return (accountsQuery.data ?? []).filter((a) => !a.hideAccount);
+  }, [accountsQuery]);
+
+  if (accountsQuery.isPending) {
+    return <SkeletonAccountCard />;
+  }
+
+  return (
+    <Card>
+      <div className="p-2">
+        <span className="w-1/2 text-2xl font-semibold tracking-tight">Net Worth</span>
+      </div>
+      <Separator />
+      <div className="flex flex-col space-y-2 p-2">
+        <NetWorthItem
+          accounts={validAccounts}
+          types={['Checking', 'Credit Card']}
+          title={'Spending'}
+        />
+        <NetWorthItem accounts={validAccounts} types={['Loan']} title={'Loans'} />
+        <NetWorthItem accounts={validAccounts} types={['Savings']} title={'Savings'} />
+        <Separator />
+        <NetWorthItem
+          accounts={validAccounts}
+          types={['Checking', 'Credit Card', 'Loan', 'Savings']}
+          title={'Liquid'}
+        />
+        <NetWorthItem
+          accounts={validAccounts}
+          types={['Investment']}
+          title={'Investments'}
+        />
+        <Separator />
+        <NetWorthItem accounts={validAccounts} title={'Total'} />
+      </div>
+    </Card>
+  );
+};
+
+export default NetWorthCard;
---
+++ client/src/app/auth/tabs/dashboard/net-worth/net-worth-item.tsx
@@ -0,0 +1,39 @@
+import { sumAccountsTotalBalance } from '@/lib/accounts';
+import { Account } from '@/types/account';
+import React from 'react';
+
+interface NetWorthItemProps {
+  accounts: Account[];
+  types?: string[];
+  title: string;
+}
+
+const NetWorthItem = (props: NetWorthItemProps): JSX.Element => {
+  const filteredAccounts = React.useMemo(() => {
+    if (props.types != null) {
+      return props.accounts.filter(
+        (a) => props.types?.includes(a.type) || props.types?.includes(a.subtype)
+      );
+    } else {
+      return props.accounts;
+    }
+  }, [props.accounts, props.types]);
+
+  return (
+    <div className="grid grid-cols-10 px-1">
+      <div className="col-span-6 text-left">
+        <span className="text-base tracking-tight">{props.title}</span>
+      </div>
+      <div className="col-span-4 text-right">
+        <span>
+          {new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+          }).format(sumAccountsTotalBalance(filteredAccounts))}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default NetWorthItem;
