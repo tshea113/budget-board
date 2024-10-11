15d52ba223828e4ebeeab6771ecc271ad358ac5f
tshea <Tylershea12@gmail.com>
2024-10-10 23:34:04 -0500

    max size for dashboard

--- client/src/app/auth/tabs/dashboard/dashboard.tsx
+++ client/src/app/auth/tabs/dashboard/dashboard.tsx
@@ -8,14 +8,14 @@ import SpendingTrendsCard from './spending-trends/spending-trends-card';
 
 const Dashboard = (): JSX.Element => {
   return (
-    <div>
+    <div className="flex w-screen flex-col items-center">
       <EmailVerified />
-      <div className="flex flex-col gap-2 lg:grid lg:grid-flow-col lg:grid-cols-10">
-        <div className="col-span-3 row-span-full space-y-2 2xl:col-span-2">
+      <div className="max-w-screen flex w-full flex-col gap-2 lg:flex-row 2xl:max-w-screen-2xl">
+        <div className="space-y-2 lg:w-2/5 lg:max-w-[425px]">
           <AccountCard />
           <NetWorthCard />
         </div>
-        <div className="col-span-7 space-y-2 2xl:col-span-8">
+        <div className="w-full space-y-2">
           <WelcomeHeader />
           <UncategorizedTransactionsCard />
           <SpendingTrendsCard />
