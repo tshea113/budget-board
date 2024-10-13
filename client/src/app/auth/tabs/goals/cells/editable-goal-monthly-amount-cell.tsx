41f7ab783a18a964cc2e1611898cdf7932ac8ecf
tshea <Tylershea12@gmail.com>
2024-10-13 18:48:16 -0500

    add editable goal monthly amount

---
+++ client/src/app/auth/tabs/goals/cells/editable-goal-monthly-amount-cell.tsx
@@ -0,0 +1,64 @@
+import { Input } from '@/components/ui/input';
+import { getMonthlyContributionTotal } from '@/lib/goals';
+import { convertNumberToCurrency } from '@/lib/utils';
+import { Goal } from '@/types/goal';
+import React from 'react';
+
+interface EditableGoalMonthlyAmountCellProps {
+  goal: Goal;
+  isSelected: boolean;
+  isError: boolean;
+  editCell: (newGoal: Goal) => void;
+}
+
+const EditableGoalMonthlyAmountCell = (
+  props: EditableGoalMonthlyAmountCellProps
+): JSX.Element => {
+  const [goalAmountValue, setGoalAmountValue] = React.useState<string>(
+    (props.goal.monthlyContribution ?? 0).toFixed(0)
+  );
+
+  React.useEffect(() => {
+    if (props.isError) {
+      (props.goal.monthlyContribution ?? 0).toFixed(0);
+    }
+  }, [props.isError]);
+
+  const onInputBlur = (): void => {
+    if (props.goal.monthlyContribution) {
+      if (!isNaN(parseFloat(goalAmountValue))) {
+        const newGoal: Goal = {
+          ...props.goal,
+          monthlyContribution: parseFloat(goalAmountValue),
+        };
+        if (props.editCell != null) {
+          props.editCell(newGoal);
+        }
+      } else {
+        setGoalAmountValue(props.goal.monthlyContribution.toFixed(0));
+      }
+    }
+  };
+
+  return (
+    <>
+      {props.isSelected && props.goal.monthlyContribution ? (
+        <Input
+          className="ml-1 h-7 w-[85px] text-center text-lg"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+          onChange={(e) => setGoalAmountValue(e.target.value)}
+          onBlur={onInputBlur}
+          value={goalAmountValue}
+        />
+      ) : (
+        <span className="font-semibold">
+          {convertNumberToCurrency(getMonthlyContributionTotal(props.goal))}
+        </span>
+      )}
+    </>
+  );
+};
+
+export default EditableGoalMonthlyAmountCell;
