bd82f6b8e70ef7a8145a8329ef0156e6d9ffcb60
tshea <Tylershea12@gmail.com>
2024-10-13 16:21:34 -0500

    Add editable goal name cell

---
+++ client/src/app/auth/tabs/goals/cells/editable-goal-name-cell.tsx
@@ -0,0 +1,54 @@
+import { Input } from '@/components/ui/input';
+import { Goal } from '@/types/goal';
+import React from 'react';
+
+interface EditableGoalNameCellProps {
+  goal: Goal;
+  isSelected: boolean;
+  isError: boolean;
+  editCell: (newGoal: Goal) => void;
+}
+
+const EditableGoalNameCell = (props: EditableGoalNameCellProps): JSX.Element => {
+  const [goalNameValue, setGoalNameValue] = React.useState(props.goal.name);
+
+  React.useEffect(() => {
+    if (props.isError) {
+      setGoalNameValue(props.goal.name);
+    }
+  }, [props.isError]);
+
+  const onGoalNameBlur = (): void => {
+    if (goalNameValue && goalNameValue.length > 0) {
+      const newGoal: Goal = {
+        ...props.goal,
+        name: goalNameValue,
+      };
+      if (props.editCell != null) {
+        props.editCell(newGoal);
+      }
+    } else {
+      setGoalNameValue(props.goal.name);
+    }
+  };
+
+  return (
+    <>
+      {props.isSelected ? (
+        <Input
+          className="h-8 max-w-[400px] text-left text-lg"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+          onChange={(e) => setGoalNameValue(e.target.value)}
+          onBlur={onGoalNameBlur}
+          value={goalNameValue}
+        />
+      ) : (
+        <span className="select-none">{props.goal.name}</span>
+      )}
+    </>
+  );
+};
+
+export default EditableGoalNameCell;
