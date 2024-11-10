e9ce3cf178b0c6d02c4e323e840743d9e34ea316
tshea <Tylershea12@gmail.com>
2024-11-09 20:18:21 -0600

    remove old todo

--- client/src/components/page-loading.tsx
+++ client/src/components/page-loading.tsx
@@ -1,7 +1,6 @@
 import LoadingIcon from './loading-icon';
 
 const PageLoading = (): JSX.Element => {
-  // TODO: This probably should be themed
   return (
     <div className="flex h-screen items-center justify-center">
       <LoadingIcon className="m-auto h-[100px] w-[100px]" />
