a0d6db9ce02a596ebee0e33a565512b97dbd3f55
tshea <Tylershea12@gmail.com>
2024-11-10 22:03:05 -0600

    Change to menu icon

--- client/src/app/auth/header.tsx
+++ client/src/app/auth/header.tsx
@@ -1,11 +1,16 @@
 import { useIsMobile } from '@/components/hooks/use-mobile';
 import { SidebarTrigger } from '@/components/ui/sidebar';
+import { MenuIcon } from 'lucide-react';
 
 const Header = (): JSX.Element => {
   const isMobile = useIsMobile();
   return (
     <div className="flex w-full flex-row items-center gap-2">
-      {isMobile && <SidebarTrigger />}
+      {isMobile && (
+        <SidebarTrigger>
+          <MenuIcon />
+        </SidebarTrigger>
+      )}
       <h2 className="grow text-3xl font-semibold tracking-tight">Budget Board</h2>
     </div>
   );
