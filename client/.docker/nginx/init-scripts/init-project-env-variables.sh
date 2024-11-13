fcc675439f8fb16124d299acb14bde8dc7e92975
tshea <Tylershea12@gmail.com>
2024-11-12 21:11:46 -0600

    Add stuff to inject api url to vite

---
+++ client/.docker/nginx/init-scripts/init-project-env-variables.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+set -ex
+
+# Find the file where environment variables need to be replaced.
+projectEnvVariables=$(ls -t /usr/share/nginx/html/assets/projectEnvVariables*.js | head -n1)
+
+# Replace environment variables
+envsubst < "$projectEnvVariables" > ./projectEnvVariables_temp
+cp ./projectEnvVariables_temp "$projectEnvVariables"
+rm ./projectEnvVariables_temp
