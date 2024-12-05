42db67d11f1e2c0bec08e97c355cee1810557dcb
tshea <Tylershea12@gmail.com>
2024-12-04 21:39:36 -0600

    fix on windows

--- client/.docker/nginx/conf/nginx-substitute.sh
+++ client/.docker/nginx/conf/nginx-substitute.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 export DOLLAR="$"
-envsubst < /etc/nginx/conf.d/custom-nginx.conf.template > /etc/nginx/conf.d/default.conf
+envsubst < default.conf.template > /etc/nginx/conf.d/default.conf
 nginx -g "daemon off;"
