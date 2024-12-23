7de413ed32447a4b7bdd0bbcea2f8de08c862aee
tshea <Tylershea12@gmail.com>
2024-12-23 16:44:24 -0600

    fix design for unbudgets

--- client/src/app/auth/pages/budget/unbudget-card.tsx
+++ client/src/app/auth/pages/budget/unbudget-card.tsx
@@ -1,22 +1,28 @@
+import ResponsiveButton from '@/components/responsive-button';
 import { Card } from '@/components/ui/card';
 import { getFormattedCategoryValue } from '@/lib/category';
+import { convertNumberToCurrency } from '@/lib/utils';
 import { transactionCategories } from '@/types/transaction';
+import { PlusIcon } from 'lucide-react';
 
 interface UnbudgetCardProps {
   name: string;
-  amount: string;
+  amount: number;
 }
 
 const UnbudgetCard = (props: UnbudgetCardProps): JSX.Element => {
   return (
-    <Card className="flex w-full flex-row px-3 py-1">
-      <div className="w-1/2 scroll-m-20 justify-self-start text-lg font-semibold tracking-tight">
+    <Card className="flex w-full flex-row justify-between px-3 py-1 @container">
+      <span className="w-2/5 text-lg font-semibold tracking-tight md:w-1/2">
         {getFormattedCategoryValue(props.name, transactionCategories)}
-      </div>
-      <div className="w-1/2">
-        <div className="w-1/3 scroll-m-20 justify-self-start text-center text-lg font-semibold tracking-tight">
-          ${props.amount}
-        </div>
+      </span>
+      <div className="flex w-3/5 flex-row items-center justify-between md:w-1/2">
+        <span className="w-1/3 text-center text-base font-semibold tracking-tight @sm:text-lg">
+          {convertNumberToCurrency(props.amount)}
+        </span>
+        <ResponsiveButton className="h-7 w-7 p-0" variant="outline" loading={false}>
+          <PlusIcon className="h-4 w-4" />
+        </ResponsiveButton>
       </div>
     </Card>
   );
