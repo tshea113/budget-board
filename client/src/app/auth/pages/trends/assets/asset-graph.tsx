d5da13599a0142c17b136dbd0d6d984da2615828
tshea113 <Tylershea12@gmail.com>
2024-12-29 16:06:26 -0500

    Add empty placeholder

--- client/src/app/auth/pages/trends/assets/asset-graph.tsx
+++ client/src/app/auth/pages/trends/assets/asset-graph.tsx
@@ -178,80 +178,89 @@ const AssetsGraph = (): JSX.Element => {
           />
         </div>
       </div>
-      <ChartContainer config={chartConfig} className="max-h-[400px] w-full">
-        <BarChart accessibilityLayer data={chartData}>
-          <CartesianGrid vertical={true} />
-          <ChartLegend content={<ChartLegendContent />} />
-          <ChartTooltip
-            cursor={false}
-            content={
-              <ChartTooltipContent
-                hideLabel
-                className="w-[220px]"
-                formatter={(value, name, item, index) => (
-                  <div className="custom-tooltip flex flex-col gap-1">
-                    <div>
-                      {new Date(item.payload.date).toLocaleDateString('en-US', {
-                        month: 'short',
-                        day: 'numeric',
-                        year: 'numeric',
-                      })}
-                    </div>
-                    <div className="flex flex-row items-center gap-1">
-                      <div
-                        className="h-2.5 w-2.5 shrink-0 rounded-[2px] bg-[--color-bg]"
-                        style={
-                          {
-                            '--color-bg': `var(--color-${name})`,
-                          } as React.CSSProperties
-                        }
-                      />
-                      {chartConfig[name as keyof typeof chartConfig]?.label || name}
-                      <div className="ml-auto flex items-baseline gap-0.5 font-mono font-medium tabular-nums text-foreground">
-                        {convertNumberToCurrency(value as number, true)}
+      {selectedAccountIds.length === 0 ? (
+        <div className="flex aspect-video max-h-[400px] w-full items-center justify-center">
+          <span className="text-center">Select an account to display the chart.</span>
+        </div>
+      ) : (
+        <ChartContainer config={chartConfig} className="max-h-[400px] w-full">
+          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={true} />
+            <ChartLegend content={<ChartLegendContent />} />
+            <ChartTooltip
+              cursor={false}
+              content={
+                <ChartTooltipContent
+                  hideLabel
+                  className="w-[220px]"
+                  formatter={(value, name, item, index) => (
+                    <div className="custom-tooltip flex flex-col gap-1">
+                      <div>
+                        {new Date(item.payload.date).toLocaleDateString('en-US', {
+                          month: 'short',
+                          day: 'numeric',
+                          year: 'numeric',
+                        })}
                       </div>
-                    </div>
-                    {/* Add this after the last item */}
-                    {index === selectedAccountIds.length - 1 && (
-                      <div className="mt-1.5 flex basis-full items-center border-t pt-1.5 text-xs font-medium text-foreground">
-                        Total
+                      <div className="flex flex-row items-center gap-1">
+                        <div
+                          className="h-2.5 w-2.5 shrink-0 rounded-[2px] bg-[--color-bg]"
+                          style={
+                            {
+                              '--color-bg': `var(--color-${name})`,
+                            } as React.CSSProperties
+                          }
+                        />
+                        {chartConfig[name as keyof typeof chartConfig]?.label || name}
                         <div className="ml-auto flex items-baseline gap-0.5 font-mono font-medium tabular-nums text-foreground">
-                          {convertNumberToCurrency(sumTooltipValues(item.payload), true)}
+                          {convertNumberToCurrency(value as number, true)}
                         </div>
                       </div>
-                    )}
-                  </div>
-                )}
-              />
-            }
-          />
-          <XAxis
-            dataKey="date"
-            tickLine={true}
-            tickMargin={15}
-            minTickGap={0}
-            axisLine={true}
-            angle={-45}
-            tickFormatter={(value) =>
-              (new Date(value).getMonth() + 1).toString() +
-              '/' +
-              new Date(value).getDate().toString()
-            }
-          />
-          <YAxis tickFormatter={(value) => convertNumberToCurrency(value as number)} />
-          {selectedAccountIds.map((accountId) => (
-            <Bar
-              key={accountId}
-              dataKey={accountId}
-              type="step"
-              fill={getChartColor(accountId, chartConfig)}
-              fillOpacity={0.4}
-              stroke={getChartColor(accountId, chartConfig)}
-              stackId="a"
+                      {/* Add this after the last item */}
+                      {index === selectedAccountIds.length - 1 && (
+                        <div className="mt-1.5 flex basis-full items-center border-t pt-1.5 text-xs font-medium text-foreground">
+                          Total
+                          <div className="ml-auto flex items-baseline gap-0.5 font-mono font-medium tabular-nums text-foreground">
+                            {convertNumberToCurrency(
+                              sumTooltipValues(item.payload),
+                              true
+                            )}
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  )}
+                />
+              }
             />
-          ))}
-        </BarChart>
-      </ChartContainer>
+            <XAxis
+              dataKey="date"
+              tickLine={true}
+              tickMargin={15}
+              minTickGap={0}
+              axisLine={true}
+              angle={-45}
+              tickFormatter={(value) =>
+                (new Date(value).getMonth() + 1).toString() +
+                '/' +
+                new Date(value).getDate().toString()
+              }
+            />
+            <YAxis tickFormatter={(value) => convertNumberToCurrency(value as number)} />
+            {selectedAccountIds.map((accountId) => (
+              <Bar
+                key={accountId}
+                dataKey={accountId}
+                type="step"
+                fill={getChartColor(accountId, chartConfig)}
+                fillOpacity={0.4}
+                stroke={getChartColor(accountId, chartConfig)}
+                stackId="a"
+              />
+            ))}
+          </BarChart>
+        </ChartContainer>
+      )}
     </div>
   );
 };
