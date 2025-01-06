1118308a1a318dfa8c9dddecb70a823b8ba66dbd
tshea <Tylershea12@gmail.com>
2025-01-05 23:17:37 -0600

    Add padding

--- client/src/app/auth/pages/transactions/transactions-configuration/custom-categories/custom-category-header.tsx
+++ client/src/app/auth/pages/transactions/transactions-configuration/custom-categories/custom-category-header.tsx
@@ -1,6 +1,6 @@
 const CustomCategoryHeader = (): JSX.Element => {
   return (
-    <div className="flex w-full flex-grow p-2">
+    <div className="flex w-full flex-grow gap-2 p-2">
       <span className="w-1/2 grow font-semibold">Name</span>
       <span className="w-1/2 grow font-semibold">Parent</span>
       <span className="w-[26px]" />
