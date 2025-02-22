883f4b2685bc44425631712fcb00a9b12ab12050
tshea <Tylershea12@gmail.com>
2025-02-22 15:38:56 -0600

    Removed depricated uid

---
+++ server/BudgetBoard.Database/Migrations/20250222202242_RemoveUserUid.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BudgetBoard.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUserUid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Uid",
+                table: "User");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Uid",
+                table: "User",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
