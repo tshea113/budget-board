96440cd396e8a05a6b7d66ee4215706475d36768
tshea <Tylershea12@gmail.com>
2025-02-27 18:57:26 -0600

    Update aliases

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@models": path.resolve(__dirname, "./src/models"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+    },
+  },
+});
