c50c9da30aa6cda8da74cadd46540f86e76f4a42
tshea <Tylershea12@gmail.com>
2025-03-19 17:13:51 -0500

    copy stuff from old

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,22 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        format: "es",
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+        manualChunks(id) {
+          if (/projectEnvVariables.ts/.test(id)) {
+            return "projectEnvVariables";
+          }
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
