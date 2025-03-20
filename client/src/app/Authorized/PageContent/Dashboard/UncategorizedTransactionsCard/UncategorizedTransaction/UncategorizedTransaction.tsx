07546dfa4b7b334b9aef149201fae07118c8bb90
tshea <Tylershea12@gmail.com>
2025-03-20 18:44:43 -0500

    remove group

--- client/src/app/Authorized/PageContent/Dashboard/UncategorizedTransactionsCard/UncategorizedTransaction/UncategorizedTransaction.tsx
+++ client/src/app/Authorized/PageContent/Dashboard/UncategorizedTransactionsCard/UncategorizedTransaction/UncategorizedTransaction.tsx
@@ -75,48 +75,46 @@ const UncategorizedTransaction = (
       bg={opened ? "var(--mantine-primary-color-light)" : ""}
     >
       <LoadingOverlay visible={doEditTransaction.isPending} />
-      <Group wrap="nowrap">
-        <Flex className={classes.container}>
-          <Flex
-            className={classes.subcontainer}
-            direction={{ base: "column", xs: "row" }}
-            style={{ flexGrow: 1 }}
-          >
-            <Text w={{ base: "100%", xs: "160px" }}>
-              {new Date(props.transaction.date).toLocaleDateString([], {
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-              })}
-            </Text>
-            <Text w="100%">{props.transaction.merchantName}</Text>
-          </Flex>
-          <Flex
-            className={classes.subcontainer}
-            direction={{ base: "column", xs: "row" }}
-            style={{ flexShrink: 1 }}
-          >
-            <EditableCategoryCell
-              transaction={props.transaction}
-              categories={props.categories}
-              isSelected={opened}
-              editCell={doEditTransaction.mutate}
-            />
-            <Text
-              w={{ base: "100%", xs: "90px" }}
-              style={{
-                color:
-                  props.transaction.amount < 0
-                    ? "var(--mantine-color-red-6)"
-                    : "var(--mantine-color-green-6)",
-                fontWeight: 600,
-              }}
-            >
-              {convertNumberToCurrency(props.transaction.amount, true)}
-            </Text>
-          </Flex>
+      <Flex className={classes.container}>
+        <Flex
+          className={classes.subcontainer}
+          direction={{ base: "column", xs: "row" }}
+          style={{ flexGrow: 1 }}
+        >
+          <Text w={{ base: "100%", xs: "160px" }}>
+            {new Date(props.transaction.date).toLocaleDateString([], {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          </Text>
+          <Text w="100%">{props.transaction.merchantName}</Text>
         </Flex>
-      </Group>
+        <Flex
+          className={classes.subcontainer}
+          direction={{ base: "column", xs: "row" }}
+          style={{ flexShrink: 1 }}
+        >
+          <EditableCategoryCell
+            transaction={props.transaction}
+            categories={props.categories}
+            isSelected={opened}
+            editCell={doEditTransaction.mutate}
+          />
+          <Text
+            w={{ base: "100%", xs: "90px" }}
+            style={{
+              color:
+                props.transaction.amount < 0
+                  ? "var(--mantine-color-red-6)"
+                  : "var(--mantine-color-green-6)",
+              fontWeight: 600,
+            }}
+          >
+            {convertNumberToCurrency(props.transaction.amount, true)}
+          </Text>
+        </Flex>
+      </Flex>
     </Card>
   );
 };
